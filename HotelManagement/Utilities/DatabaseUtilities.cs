8d77a44a28ee2c8a489b5d918f55e2db155b617c
DESKTOP-HRO9T40\Admin <lenhattuan.it.hcmus@gmail.com>
2021-06-13 09:24:15 +0700

    adding database utilities

---
+++ HotelManagement/Utilities/DatabaseUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Utilities
+{
+    class DatabaseUtilities
+    {
+        private DatabaseUtilities() { }
+
+        private static DatabaseUtilities _databaseInstance;
+        private static HotelManagementEntities _databaseHotelManagement;
+
+        public static DatabaseUtilities GetDatabaseInstance()
+        {
+            if (_databaseInstance == null)
+            {
+                _databaseInstance = new DatabaseUtilities();
+                _databaseHotelManagement = new HotelManagementEntities();
+            }
+            else
+            {
+                //Do Nothing
+            }
+
+            return _databaseInstance;
+        }
+
+
+    }
+}
